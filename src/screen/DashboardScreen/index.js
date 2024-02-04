bf7ece5fea91e2efd8ecbddd176f614135b843ab
LeoZai613 <102532972+LeoZai613@users.noreply.github.com>
2024-02-04 04:13:16 -0500

    Stylizing

--- src/screen/DashboardScreen/index.js
+++ src/screen/DashboardScreen/index.js
@@ -1,5 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList, Image, Text, View, Button} from 'react-native';
+import {
+  FlatList,
+  Image,
+  Text,
+  View,
+  Button,
+  ImageBackground,
+} from 'react-native';
 import axios from 'axios';
 
 const Dashboard = ({navigation}) => {
@@ -27,37 +34,53 @@ const Dashboard = ({navigation}) => {
   };
 
   return (
-    <View>
-      <Button title="Logout" onPress={handleLogout} />
-      <FlatList
-        data={pokemonData}
-        keyExtractor={item => item.name}
-        renderItem={({item}) => (
-          <View
-            style={{
-              borderBottomWidth: 1,
-              borderBottomColor: 'grey',
-              padding: 10,
-            }}>
-            <Text style={{fontSize: 18, fontWeight: 'bold'}}>{item.name}</Text>
-            <Image
-              source={{uri: item.sprites.front_default}}
-              style={{width: 100, height: 100}}
-            />
-            <Text>
-              Type: {item.types.map(typeInfo => typeInfo.type.name).join(', ')}
-            </Text>
-            <Text>
-              Attacks:{' '}
-              {item.moves
-                .slice(0, 4)
-                .map(moveInfo => moveInfo.move.name)
-                .join(', ')}
-            </Text>
-          </View>
-        )}
-      />
-    </View>
+    <ImageBackground
+      source={{
+        uri: 'https://w0.peakpx.com/wallpaper/204/837/HD-wallpaper-gengar-ghost-pokemon.jpg',
+      }}
+      style={{flex: 1}}>
+      <View>
+        <Button title="Logout" onPress={handleLogout} />
+        <FlatList
+          data={pokemonData}
+          keyExtractor={item => item.name}
+          renderItem={({item}) => (
+            <View
+              style={{
+                borderBottomWidth: 1,
+                borderBottomColor: 'grey',
+                padding: 10,
+                // backgroundColor: 'rgba(255, 255, 255, 0.5)', // Adjust alpha value for transparency
+              }}>
+              <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+                {item.name}
+              </Text>
+              <Image
+                source={{uri: item.sprites.front_default}}
+                style={{width: 100, height: 100}}
+              />
+              <Text>
+                Type:{' '}
+                {item.types.map(typeInfo => typeInfo.type.name).join(', ')}
+              </Text>
+              <Text>
+                Abilities:{' '}
+                {item.abilities
+                  .map(abilityInfo => abilityInfo.ability.name)
+                  .join(', ')}
+              </Text>
+              <Text>
+                Moves:{' '}
+                {item.moves
+                  .slice(0, 4)
+                  .map(moveInfo => moveInfo.move.name)
+                  .join(', ')}
+              </Text>
+            </View>
+          )}
+        />
+      </View>
+    </ImageBackground>
   );
 };
 
