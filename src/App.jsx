03ad4a93d3412148eb8b5b70db6f9bd54c8ad415
LeoZai613 <102532972+LeoZai613@users.noreply.github.com>
2024-02-26 06:39:44 -0500

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Navigation from './navigation';
 import {AppContextProvider} from './contexts/AppContext';
+import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
+  useEffect(() => {
+    const requestUserPermission = async () => {
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+      if (enabled) {
+        console.log('Authorization status:', authStatus);
+        const token = await messaging().getToken();
+        console.log('FCM Token:', token);
+      }
+    };
+
+    requestUserPermission();
+
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <AppContextProvider>
       <NavigationContainer>
