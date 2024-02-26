6bfb5b795ffe549dbb2e1ac45c58d64456f16381
LeoZai613 <102532972+LeoZai613@users.noreply.github.com>
2024-02-25 22:27:16 -0500

    Your commit message here

---
+++ android/app/src/FireBaseNotification/FireBaseNotification.java
@@ -0,0 +1,40 @@
+package FireBaseNotification;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import android.util.Log;
+
+public class FireBaseNotification extends FirebaseMessagingService {
+
+    private static final String TAG = "FireBaseNotification";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated.
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+    }
+
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // FCM registration token to your app server.
+        sendRegistrationToServer(token);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        // TODO: Implement this method to send token to your app server.
+    }
+}
